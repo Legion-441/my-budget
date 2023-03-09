87587ad81cb87e571b442c274e5ce8a7bec8d6c3
Legion-441 <piotrbartnicki1995@gmail.com>
2023-03-10 00:27:07 +0100

    puting pages into variable

---
+++ src/components/nav-bar/nav-pages.tsx
@@ -0,0 +1,12 @@
+//! Icons
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import HistoryIcon from '@mui/icons-material/History';
+import SettingsIcon from '@mui/icons-material/Settings';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+export const pages = [
+  { icon: <DashboardIcon />, label: 'Dashboard', path: '/dash' },
+  { icon: <HistoryIcon />, label: 'History', path: '/history' },
+  { icon: <SettingsIcon />, label: 'Settings', path: '/settings' }
+]
