498df078b4e97a23e7ba40fac1f15ed9cf0fa851
Legion-441 <piotrbartnicki1995@gmail.com>
2023-05-19 17:17:31 +0200

    adding darkmode switch

---
+++ src/components/nav-bar/darkModeSwitch.tsx
@@ -0,0 +1,26 @@
+import { Switch } from "@mui/material"
+import { useAppDispatch, useAppSelector } from "../../app/hooks"
+import { selectAppColorMode, setAppColorMode } from "../../slices/app/app.slice"
+
+export const DarkModeSwitch: React.FC = () => {
+  const dispatch = useAppDispatch()
+  const appColorMode = useAppSelector(selectAppColorMode)
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setAppColorMode(event.target.checked ? 'dark' : 'light'));
+  };
+
+
+  return (<>
+    <Switch
+      checked={appColorMode === 'dark'}
+      onChange={handleChange}
+      inputProps={{ 'aria-label': 'toggle dark mode' }}
+    />
+    {appColorMode} mode
+    </>
+  );
+
+  
+}
+
