85e16d874721e31d308572ff5613ebc5528b6242
Legion-441 <piotrbartnicki1995@gmail.com>
2023-05-21 17:55:47 +0200

    start making history page template

--- src/views/history/history.tsx
+++ src/views/history/history.tsx
@@ -1,23 +1,132 @@
+import * as React from 'react';
+import { Box, Collapse, IconButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import Paper from "@mui/material/Paper"
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
+
+function createData(
+  date: string,
+  name: string,
+  category: string,
+  subcategory: string,
+  price: number,
+) {
+  return {
+    date,
+    name,
+    category,
+    subcategory,
+    price,
+    history: [
+      {
+        date: '2020-01-05',
+        customerId: '11091700',
+        amount: 3,
+      },
+      {
+        date: '2020-01-02',
+        customerId: 'Anonymous',
+        amount: 1,
+      },
+    ],
+  };
+}
+
+function Row(props: { row: ReturnType<typeof createData> }) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.date}
+        </TableCell>
+        <TableCell align="right">{row.name}</TableCell>
+        <TableCell align="right">{row.category}</TableCell>
+        <TableCell align="right">{row.subcategory}</TableCell>
+        <TableCell align="right">{row.price}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Typography variant="h6" gutterBottom component="div">
+                History
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Date</TableCell>
+                    <TableCell>Customer</TableCell>
+                    <TableCell align="right">Amount</TableCell>
+                    <TableCell align="right">Total price ($)</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {row.history.map((historyRow) => (
+                    <TableRow key={historyRow.date}>
+                      <TableCell component="th" scope="row">
+                        {historyRow.date}
+                      </TableCell>
+                      <TableCell>{historyRow.customerId}</TableCell>
+                      <TableCell align="right">{historyRow.amount}</TableCell>
+                      <TableCell align="right">
+                        {Math.round(historyRow.amount * row.price * 100) / 100}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+
+const rows = [
+  createData('1/1/2023', 'Sandwich', 'Grocery', 'Meal', 25.99),
+  createData('2/1/2023', 'Fuel', 'Transport', 'Car', 5.00),
+  createData('2/1/2023', 'Glass', 'Home', 'Dish', 18.99),
+  createData('4/1/2023', 'Chair', 'Home', 'Furniture', 6.99),
+  createData('5/1/2023', 'Sandwich', 'Grocery', 'Meal', 10.00),
+  createData('5/1/2023', 'Sandwich', 'Grocery', 'Meal', 21.50),
+];
 
 const HistoryView: React.FC  = () => {
-  return <>
-    <Paper sx={{p: 2}} >
-      HistoryView <br/>
-      HistoryView <br/>
-      HistoryView <br/>
-      HistoryView <br/>
-      HistoryView <br/>
-      HistoryView <br/>
-      HistoryView <br/>
-      HistoryView <br/>
-      HistoryView <br/>
-      HistoryView <br/>
-      HistoryView <br/>
-      HistoryView <br/>
-      HistoryView <br/>
-    </Paper>
-  </>
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell>Date</TableCell>
+            <TableCell align="right">Name</TableCell>
+            <TableCell align="right">Category</TableCell>
+            <TableCell align="right">Subcategory</TableCell>
+            <TableCell align="right">Price</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <Row key={row.name} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
 
 export default HistoryView
