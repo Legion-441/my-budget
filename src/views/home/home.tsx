5de3eca82c4c7d08ac09f2cfa966a624d9403a88
Legion-441 <piotrbartnicki1995@gmail.com>
2023-06-10 11:05:59 +0200

    Added welcome page

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,7 +1,24 @@
+import { useAppSelector } from "../../app/hooks"
+import { selectUsername } from "../../slices/app/app.slice"
+//* MUI & Icons
+import { Button, Stack } from "@mui/material"
+import { Group, Person } from "@mui/icons-material"
+//* Styled Components
 import PaperCard from "../../styled/paper-card/paper-card.styled"
 
 const HomeView: React.FC  = () => {
-  return <PaperCard>HomeView</PaperCard>
+  const username = useAppSelector(selectUsername)
+  
+  return <PaperCard>
+    <h1>Witaj{username ? `, ${username}` : ""}!</h1>
+    <p>Wygląda na to, że nie masz jeszcze żadnego założonego budżetu.</p>
+    <h2>Aby rozpocząć, wybierz jedną z opcji:</h2>
+    <Stack spacing={2} direction="row">
+      <Button variant="contained" startIcon={<Person/>}>Stwórz nowy</Button>
+      <Button variant="outlined" startIcon={<Group/>}>Dołącz do kooperacji</Button>
+    </Stack>
+    <p>Dziękujemy za korzystanie z naszej strony do śledzenia budżetu i życzymy Ci sukcesów w zarządzaniu finansami!</p>
+  </PaperCard>
 }
 
 export default HomeView
