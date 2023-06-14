6d427541ac23672799a769456d990413b536437d
Legion-441 <piotrbartnicki1995@gmail.com>
2023-06-14 17:26:23 +0200

    app-header: used useEffect

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import { selectUserInfo, toggleDrawer, toggleTempDrawer } from '../../slices/app/app.slice';
+import { BudgetsListItem, selectPickedBudgetId, selectUserInfo, toggleDrawer, toggleTempDrawer } from '../../slices/app/app.slice';
 //* Utils
 import { getIconComponent } from '../../utils/iconUtils';
 //* MUI
@@ -11,20 +12,24 @@ import { ExpandMore, ExpandLess, Menu, Mail, Notifications, AccountCircle } from
 import AppProfileMenu from './profile-menu';
 import AppBudgetsMenu from './budgets-menu';
 
-interface AppHeaderProps {
-  budgetId: string | undefined
-}
-const AppHeader: React.FC<AppHeaderProps> = ({budgetId}) => {
-  const [profileAnchorEl, setProfileAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [budgetAnchorEl, setBudgetAnchorEl] = React.useState<null | HTMLElement>(null);
-  
-  const dispatch = useAppDispatch()
-  
-  const { budgetsList } = useAppSelector(selectUserInfo)
-  const selectedBudget = budgetsList.find((budget) => budget.id === budgetId)
 
+const AppHeader: React.FC = () => {
+  const [profileAnchorEl, setProfileAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [budgetAnchorEl, setBudgetAnchorEl] = React.useState<null | HTMLElement>(null); 
+  const [selectedBudget, setSelectedBudget] = React.useState<null | BudgetsListItem>(null); 
   const theme = useTheme();
-  const isLargeScreen = useMediaQuery(theme.breakpoints.up('lg'));
+  const dispatch = useAppDispatch()
+  const budgetId = useAppSelector(selectPickedBudgetId)
+  const { budgetsList } = useAppSelector(selectUserInfo)
+  
+  useEffect(() => {
+    const selectedBudget = budgetsList.find((budget) => budget.id === budgetId) || null
+    setSelectedBudget(selectedBudget)
+
+  }, [budgetsList, budgetId])
+  
+  
+  const isLargeScreen = useMediaQuery(theme.breakpoints.up('lg'));  
   
   const handleDrawerToggle = () => {
     dispatch(isLargeScreen ? toggleDrawer() : toggleTempDrawer())
@@ -41,14 +46,14 @@ const AppHeader: React.FC<AppHeaderProps> = ({budgetId}) => {
   const menuId = 'primary-search-account-menu';
 
   return (
-    <>
-      <Box sx={{ flexGrow: 0 }}>
-        <AppBar position="fixed" sx={{zIndex: (theme) => theme.zIndex.drawer + 1}}>
-          <Toolbar>
-            <Box sx={{
-                display: { sm: 'flex', xs: 'none' },
-                alignItems: 'center'
-              }}>
+    <Box sx={{ flexGrow: 0 }}>
+      <AppBar position="fixed" sx={{zIndex: (theme) => theme.zIndex.drawer + 1}}>
+        <Toolbar>
+          <Box sx={{
+              display: { sm: 'flex', xs: 'none' },
+              alignItems: 'center'
+            }}>
+            {budgetId && (
               <IconButton
                 size="large"
                 edge="start"
@@ -59,77 +64,79 @@ const AppHeader: React.FC<AppHeaderProps> = ({budgetId}) => {
               >
                 <Menu />
               </IconButton>
-              <img src="/Logo.svg" alt="Logo" style={{ height: '40px', marginRight: '10px' }} />
-              <Typography
-                variant="h6"
-                noWrap
-                component="div"
-                sx={{ display: 'block' }}
-              >
-                myBudget
-              </Typography>
-            </Box>
-            <Box sx={{ flexGrow: {sm: 1, xs: 0} }} />
-            <Button
-              id='BudgetButton'
-              color="inherit"
-              variant="outlined"
-              disabled={budgetsList.length == 0}
-              onClick={handleToggleBudgetsMenu}
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                flexGrow: {sm: 0, xs: 1},
-                minWidth: {sm: 200, xs: 80}
-              }}
-              startIcon={selectedBudget && getIconComponent(selectedBudget.icon)}
-              endIcon={budgetAnchorEl===null ? <ExpandMore /> : <ExpandLess />}
+              )
+            }
+            <img src="/Logo.svg" alt="Logo" style={{ height: '40px', marginRight: '10px' }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ display: 'block' }}
             >
-              <Typography
-                noWrap={true}
-                variant="body1"
-                fontSize={"medium"}
-                overflow='hidden'
-                textOverflow='ellipsis'
-                flexGrow={1}
-                textAlign={'left'}
-              >
-                {selectedBudget ? selectedBudget.name : 'Wybierz budżet...'}
-              </Typography>
-            </Button>
-            <Box sx={{ display: 'flex' }}>
-              <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-                <Badge badgeContent={4} color="error">
-                  <Mail />
-                </Badge>
-              </IconButton>
-              <IconButton
-                size="large"
-                aria-label="show 17 new notifications"
-                color="inherit"
-              >
-                <Badge badgeContent={17} color="error">
-                  <Notifications />
-                </Badge>
-              </IconButton>
-              <IconButton
-                size="large"
-                edge="end"
-                aria-label="account of current user"
-                aria-controls={menuId}
-                aria-haspopup="true"
-                onClick={handleToggleProfileMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-            </Box>
-          </Toolbar>
-        </AppBar>
-        <AppBudgetsMenu anchorEl={budgetAnchorEl} handleToggleBudgetsMenu={handleToggleBudgetsMenu}/>
-        <AppProfileMenu anchorEl={profileAnchorEl} handleToggleProfileMenu={handleToggleProfileMenu}/>
-      </Box>
-    </>
+              myBudget
+            </Typography>
+          </Box>
+          <Box sx={{ flexGrow: {sm: 1, xs: 0} }} />
+          <Button
+            id='BudgetButton'
+            aria-label='Wybierz budżet'
+            color="inherit"
+            variant="outlined"
+            disabled={budgetsList.length === 0}
+            onClick={handleToggleBudgetsMenu}
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              flexGrow: {sm: 0, xs: 1},
+              minWidth: {sm: 200, xs: 80}
+            }}
+            startIcon={selectedBudget && getIconComponent(selectedBudget.icon)}
+            endIcon={budgetAnchorEl===null ? <ExpandMore /> : <ExpandLess />}
+          >
+            <Typography
+              noWrap={true}
+              variant="body1"
+              fontSize={"medium"}
+              overflow='hidden'
+              textOverflow='ellipsis'
+              flexGrow={1}
+              textAlign={'left'}
+            >
+              {selectedBudget ? selectedBudget.name : 'Wybierz budżet...'}
+            </Typography>
+          </Button>
+          <Box sx={{ display: 'flex' }}>
+            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="error">
+                <Mail />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Badge badgeContent={17} color="error">
+                <Notifications />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleToggleProfileMenu}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <AppBudgetsMenu anchorEl={budgetAnchorEl} handleToggleBudgetsMenu={handleToggleBudgetsMenu}/>
+      <AppProfileMenu anchorEl={profileAnchorEl} handleToggleProfileMenu={handleToggleProfileMenu}/>
+    </Box>
   );
 }
 
