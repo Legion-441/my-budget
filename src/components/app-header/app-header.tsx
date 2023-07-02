d00399e5b3581873f866e8bb613043c3c4d3aad6
Legion-441 <piotrbartnicki1995@gmail.com>
2023-07-02 19:18:33 +0200

    move budget-list button to new component

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -2,15 +2,14 @@ import * as React from 'react';
 import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { BudgetsListItem, selectPickedBudgetId, selectUserInfo, toggleDrawer, toggleTempDrawer } from '../../slices/app/app.slice';
-//* Utils
-import { getIconComponent } from '../../utils/iconUtils';
 //* MUI
 import { AppBar, Box, Toolbar, IconButton, Typography, Badge, Button } from '@mui/material';
 import { useMediaQuery, useTheme } from '@mui/material';
-import { ExpandMore, ExpandLess, Menu, Mail, Notifications, AccountCircle } from '@mui/icons-material';
+import { Menu, Mail, Notifications, AccountCircle } from '@mui/icons-material';
 //* Components
 import AppProfileMenu from './profile-menu';
 import AppBudgetsMenu from './budgets-menu';
+import BudgetListButton from './budgetList-Button';
 
 
 const AppHeader: React.FC = () => {
@@ -44,15 +43,16 @@ const AppHeader: React.FC = () => {
   };
 
   const menuId = 'primary-search-account-menu';
+  
 
   return (
     <Box sx={{ flexGrow: 0 }}>
       <AppBar position="fixed" sx={{zIndex: (theme) => theme.zIndex.drawer + 1}}>
         <Toolbar>
           <Box sx={{
-              display: { sm: 'flex', xs: 'none' },
-              alignItems: 'center'
-            }}>
+            display: { sm: 'flex', xs: 'none' },
+            alignItems: 'center'
+          }}>
             {budgetId && (
               <IconButton
                 size="large"
@@ -64,8 +64,7 @@ const AppHeader: React.FC = () => {
               >
                 <Menu />
               </IconButton>
-              )
-            }
+            )}
             <img src="/Logo.svg" alt="Logo" style={{ height: '40px', marginRight: '10px' }} />
             <Typography
               variant="h6"
@@ -77,34 +76,12 @@ const AppHeader: React.FC = () => {
             </Typography>
           </Box>
           <Box sx={{ flexGrow: {sm: 1, xs: 0} }} />
-          <Button
-            id='BudgetButton'
-            aria-label='Wybierz budżet'
-            color="inherit"
-            variant="outlined"
+          <BudgetListButton 
             disabled={budgetsList.length === 0}
-            onClick={handleToggleBudgetsMenu}
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              flexGrow: {sm: 0, xs: 1},
-              minWidth: {sm: 200, xs: 80}
-            }}
-            startIcon={selectedBudget && getIconComponent(selectedBudget.icon)}
-            endIcon={budgetAnchorEl===null ? <ExpandMore /> : <ExpandLess />}
-          >
-            <Typography
-              noWrap={true}
-              variant="body1"
-              fontSize={"medium"}
-              overflow='hidden'
-              textOverflow='ellipsis'
-              flexGrow={1}
-              textAlign={'left'}
-            >
-              {selectedBudget ? selectedBudget.name : 'Wybierz budżet...'}
-            </Typography>
-          </Button>
+            isOpen={budgetAnchorEl!==null}
+            handleToggleBudgetsMenu={handleToggleBudgetsMenu}
+            selectedBudget={selectedBudget} 
+          />
           <Box sx={{ display: 'flex' }}>
             <IconButton size="large" aria-label="show 4 new mails" color="inherit">
               <Badge badgeContent={4} color="error">
---
+++ src/components/app-header/budgetList-Button.tsx
@@ -0,0 +1,48 @@
+//* MUI
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
+import { Button, Typography } from "@mui/material";
+//* Utils
+import { getIconComponent } from "../../utils/iconUtils";
+import { BudgetsListItem } from "../../slices/app/app.slice";
+
+interface BudgetListButtonProps {
+  disabled: boolean;
+  isOpen: boolean
+  handleToggleBudgetsMenu: () => void;
+  selectedBudget: null | BudgetsListItem;
+}
+
+const BudgetListButton: React.FC<BudgetListButtonProps> = ({disabled, isOpen, handleToggleBudgetsMenu, selectedBudget }) => {
+  return (
+    <Button
+      id='BudgetButton'
+      aria-label='Wybierz budżet'
+      color="inherit"
+      variant="outlined"
+      disabled={disabled}
+      onClick={handleToggleBudgetsMenu}
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        flexGrow: {sm: 0, xs: 1},
+        minWidth: {sm: 200, xs: 80}
+      }}
+      startIcon={selectedBudget?.icon && getIconComponent(selectedBudget.icon)}
+      endIcon={isOpen ? <ExpandLess /> : <ExpandMore />}
+    >
+      <Typography
+        noWrap={true}
+        variant="body1"
+        fontSize={"medium"}
+        overflow='hidden'
+        textOverflow='ellipsis'
+        flexGrow={1}
+        textAlign={'left'}
+      >
+        {selectedBudget?.name || 'Wybierz budżet...'}
+      </Typography>
+    </Button>
+  )
+}
+
+export default BudgetListButton;
