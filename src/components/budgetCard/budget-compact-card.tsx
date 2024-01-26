c81f7ff9174d80d734762e3b58ade6f9483ba8e5
Legion-441 <piotrbartnicki1995@gmail.com>
2024-01-26 22:04:35 +0100

    changing compact card onHover styling

--- src/components/budgetCard/budget-compact-card.tsx
+++ src/components/budgetCard/budget-compact-card.tsx
@@ -9,17 +9,6 @@ import BudgetPinButton from "./budget-card-pin-button";
 //* Types
 import { AppBudgetMetaData } from "../../types/AppTypes";
 
-const StyledCard = styled(Card)({
-  cursor: "pointer",
-  WebkitTapHighlightColor: "transparent",
-  WebkitTouchCallout: "none",
-  WebkitUserSelect: "none",
-  KhtmlUserSelect: "none",
-  MozUserSelect: "none",
-  msUserSelect: "none",
-  userSelect: "none",
-});
-
 interface BudgetCompactCardProps {
   budget: AppBudgetMetaData;
   handleMenuOpen: (event: React.MouseEvent<HTMLElement>, pickedBudget: AppBudgetMetaData) => void;
@@ -31,9 +20,6 @@ const BudgetCompactCard: React.FC<BudgetCompactCardProps> = ({ budget, handleMen
   const navigate = useNavigate();
 
   const navigateToBudgetPage = () => navigate(`/budget/${budget.id}/dash`);
-
-  const cardElevation = isHover ? 10 : 6;
-  const cardBackgroundColor = isHover ? theme.palette.action.focus : "initial";
 
   const actionButton = (
     <>
@@ -63,40 +49,41 @@ const BudgetCompactCard: React.FC<BudgetCompactCardProps> = ({ budget, handleMen
   );
 
   return (
-    <StyledCard
-      elevation={cardElevation}
+    <Card
+      elevation={6}
       onClick={navigateToBudgetPage}
-      sx={{ backgroundColor: cardBackgroundColor }}
       onPointerOver={() => setIsHover(true)}
       onPointerOut={() => setIsHover(false)}
     >
-      <CardHeader
-        sx={{ paddingBottom: 0 }}
-        avatar={
-          <Avatar aria-label="icon" variant="rounded" sx={{ bgcolor: theme.palette.secondary.main }}>
-            <BudgetIconComponent iconName={budget.icon} />
-          </Avatar>
-        }
-        action={actionButton}
-        title={budget.name}
-        subheader={
-          <Box display={"flex"} gap={1}>
-            {budget.owner.username}
-            <Divider orientation="vertical" flexItem />
-            {new Date(budget.createdAt).toLocaleDateString()}
-          </Box>
-        }
-      />
+      <CardActionArea>
+        <CardHeader
+          sx={{ paddingBottom: 0 }}
+          avatar={
+            <Avatar aria-label="icon" variant="rounded" sx={{ bgcolor: theme.palette.secondary.main }}>
+              <BudgetIconComponent iconName={budget.icon} />
+            </Avatar>
+          }
+          action={actionButton}
+          title={budget.name}
+          subheader={
+            <Box display={"flex"} gap={1}>
+              {budget.owner.username}
+              <Divider orientation="vertical" flexItem />
+              {new Date(budget.createdAt).toLocaleDateString()}
+            </Box>
+          }
+        />
 
-      <CardContent>
-        <Box gap={1}>
-          <Typography component={"span"} variant="body2" color={budget.description ? "text.secondary" : "text.disabled"}>
-            {budget.description ? budget.description : "Brak opisu"}
-          </Typography>
-          {budget.members.length > 0 && <MembersCountInfo />}
-        </Box>
-      </CardContent>
-    </StyledCard>
+        <CardContent>
+          <Box gap={1}>
+            <Typography component={"span"} variant="body2" color={budget.description ? "text.secondary" : "text.disabled"}>
+              {budget.description ? budget.description : "Brak opisu"}
+            </Typography>
+            {budget.members.length > 0 && <MembersCountInfo />}
+          </Box>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 };
 
