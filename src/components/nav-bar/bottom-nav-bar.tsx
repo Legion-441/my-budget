d9e96c7e01339a6bdd9e381c44c462db7b0b25b1
Legion-441 <piotrbartnicki1995@gmail.com>
2024-01-28 21:11:36 +0100

    Optimizing bottom nav bar and handling links

--- src/components/nav-bar/bottom-nav-bar.tsx
+++ src/components/nav-bar/bottom-nav-bar.tsx
@@ -1,50 +1,39 @@
-import * as React from 'react';
-import { useNavigate } from 'react-router-dom';
-//* PAGES
-import { navLinks } from './nav-pages';
+import * as React from "react";
+//* Pages
+import { navLinks } from "./nav-pages";
 //* MUI
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
+import { BottomNavigation, BottomNavigationAction, Paper } from "@mui/material";
+//* Styled Components
+import UnstyledLink from "../../styled/unstyled-link/unstyled-link.styled";
 
 interface MobileNavBarProps {
-  selectedSubPage: number | undefined,
-  pickedBudgetID: string
+  selectedSubPage: number | undefined;
+  pickedBudgetID: string;
 }
 
-const MobileBottomNavigation: React.FC<MobileNavBarProps> = ({ selectedSubPage, pickedBudgetID}) => {
-  const ref = React.useRef<HTMLDivElement>(null);
-  const navigate = useNavigate();
-  
-  const NavActionButtons: JSX.Element[] = navLinks.map((item) => (
-    <BottomNavigationAction 
-      key={item.label}
-      label={item.label}
-      icon={item.icon}
-      onClick={() => item.subPath && navigate(`/budget/${pickedBudgetID}/${item.subPath}`)} />
-  ))
-  
-// TODO: Implement elevation of bottom navbar in dark mode
+const MobileBottomNavigation: React.FC<MobileNavBarProps> = ({ selectedSubPage, pickedBudgetID }) => {
+  // TODO: Implement elevation of bottom navbar in dark mode
+  // !Fixme: no clickable navbar when page is scroll down
   return (
-    <Box sx={{
-        display: { xs: 'inherit', sm: 'none' },
-        pb: 7 
-      }} ref={ref}>
-      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} square={true} elevation={4}>
-        <BottomNavigation
-          showLabels
-          value={selectedSubPage}
-          sx={{
-            bgcolor: 'transparent',
-            color: "red"
-          }}
-        >
-          {NavActionButtons}
-        </BottomNavigation>
-      </Paper>
-    </Box>
+    <BottomNavigation
+      component={Paper}
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0, display: { xs: "inherit", sm: "none" } }}
+      square={true}
+      elevation={4}
+      showLabels
+      value={selectedSubPage}
+    >
+      {navLinks.map((item) => (
+        <BottomNavigationAction
+          component={UnstyledLink}
+          to={`/budget/${pickedBudgetID}/${item.subPath}`}
+          key={item.label}
+          label={item.label}
+          icon={item.icon}
+        />
+      ))}
+    </BottomNavigation>
   );
-}
+};
 
-export default MobileBottomNavigation
+export default MobileBottomNavigation;
