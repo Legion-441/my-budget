111698cd7e0eb9398a4af058e53fba44df8ef443
Legion-441 <piotrbartnicki1995@gmail.com>
2024-02-02 01:08:33 +0100

    Fixing non clickable bottom navbar

--- src/components/nav-bar/bottom-nav-bar.tsx
+++ src/components/nav-bar/bottom-nav-bar.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 //* Pages
 import { navLinks } from "./nav-pages";
 //* MUI
-import { BottomNavigation, BottomNavigationAction, Paper } from "@mui/material";
+import { BottomNavigation, BottomNavigationAction, Paper, Toolbar } from "@mui/material";
 //* Styled Components
 import UnstyledLink from "../../styled/unstyled-link/unstyled-link.styled";
 
@@ -12,27 +12,28 @@ interface MobileNavBarProps {
 }
 
 const MobileBottomNavigation: React.FC<MobileNavBarProps> = ({ selectedSubPage, pickedBudgetID }) => {
-  // TODO: Implement elevation of bottom navbar in dark mode
-  // !Fixme: no clickable navbar when page is scroll down
   return (
-    <BottomNavigation
-      component={Paper}
-      sx={{ position: "fixed", bottom: 0, left: 0, right: 0, display: { xs: "inherit", sm: "none" } }}
-      square={true}
-      elevation={4}
-      showLabels
-      value={selectedSubPage}
-    >
-      {navLinks.map((item) => (
-        <BottomNavigationAction
-          component={UnstyledLink}
-          to={`/budget/${pickedBudgetID}/${item.subPath}`}
-          key={item.label}
-          label={item.label}
-          icon={item.icon}
-        />
-      ))}
-    </BottomNavigation>
+    <>
+      <Toolbar />
+      <BottomNavigation
+        component={Paper}
+        sx={{ position: "fixed", bottom: 0, left: 0, right: 0, display: { xs: "flex", sm: "none" } }}
+        square={true}
+        elevation={4}
+        showLabels
+        value={selectedSubPage}
+      >
+        {navLinks.map((item) => (
+          <BottomNavigationAction
+            component={UnstyledLink}
+            to={`/budget/${pickedBudgetID}/${item.subPath}`}
+            key={item.label}
+            label={item.label}
+            icon={item.icon}
+          />
+        ))}
+      </BottomNavigation>
+    </>
   );
 };
 
